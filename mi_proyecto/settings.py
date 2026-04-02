c08f340f53d1b9a35106f13fcfd86e43a9b6fb93
Delta <elverdaderomaxitog235374@gmail.com>
2026-04-02 16:12:22 -0600

    mm

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'mi_proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')], # Esto es clave
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Esto es clave
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
